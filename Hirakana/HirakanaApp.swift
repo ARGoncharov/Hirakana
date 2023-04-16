1b5735dd959a08a736a55ff0ebdd533c19c158d4
ARGoncharov <gonlex@mail.ru>
2023-04-16 15:04:57 +0300

    Initial Commit

---
+++ Hirakana/HirakanaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HirakanaApp.swift
+//  Hirakana
+//
+//  Created by Алексей  on 16.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct HirakanaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
